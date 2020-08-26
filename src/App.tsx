25e150c5d230c287cee7c764a01ecf12557d15ad
Takeshi Yaeda <t.yaeda@gmail.com>
2020-08-26 22:36:27 +0900

    feat: show lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 import { useDropzone } from "react-dropzone";
+import Lottie from "react-lottie";
 
 function App() {
+  const [lottieData, setLottieData] = useState<Object[]>([]);
   const onDrop = useCallback((acceptedFiles: Blob[]) => {
-    console.log(acceptedFiles);
+    acceptedFiles.forEach((file: Blob) => {
+      const reader = new FileReader();
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        try {
+          const data = JSON.parse(reader.result as string);
+          setLottieData([data]);
+        } catch {
+          // TODO: show error in UI
+        }
+      };
+      reader.readAsText(file);
+    });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: "application/json",
+    // TODO: support multiple files
     multiple: false,
   });
 
@@ -17,6 +33,19 @@ function App() {
       <input {...getInputProps()} />
       {isDragActive ? (
         <p>Drop the files here ...</p>
+      ) : lottieData.length > 0 ? (
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: lottieData[0],
+            rendererSettings: {
+              preserveAspectRatio: "xMidYMid slice",
+            },
+          }}
+          width={400}
+          height={400}
+        />
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
       )}
